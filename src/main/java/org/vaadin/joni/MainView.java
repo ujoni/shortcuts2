c35cd79e838355a622352292f312925f6dbdca34
ujoni <joni@vaadin.com>
2018-12-12 17:12:24 +0200

    Added some documentation for ShortcutUtil.java

--- src/main/java/org/vaadin/joni/MainView.java
+++ src/main/java/org/vaadin/joni/MainView.java
@@ -3,6 +3,7 @@ package org.vaadin.joni;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.html.Nav;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -18,10 +19,12 @@ import org.vaadin.joni.shortcututil.ShortcutUtil;
 public class MainView extends VerticalLayout implements RouterLayout {
     public MainView() {
         Nav nav = new Nav();
-
+        nav.setSizeFull();
         nav.add(new RouterLink("Route One", RouteOne.class));
+        nav.add(new Span(" | "));
         nav.add(new RouterLink("Route Two", RouteTwo.class));
-
+        nav.add(new Span(" | "));
+        nav.add(new RouterLink("Route Three", RouteThree.class));
 
         add(nav, new Paragraph("Welcome to the awesome shortcut page!"));
 
--- src/main/java/org/vaadin/joni/shortcututil/ShortcutUtil.java
+++ src/main/java/org/vaadin/joni/shortcututil/ShortcutUtil.java
@@ -11,11 +11,30 @@ import static org.vaadin.marcus.shortcut.Shortcut.*;
 
 
 public final class ShortcutUtil {
-
+    /*
+        This is a map from makeId(Key...) -> list of HandlerHolders
+        Handler holders contain the shortcut owning Component and a Listener
+     */
     private static HashMap<String, ArrayDeque<HandlerHolder>> scHolders = new HashMap<>();
+    /*
+        This is a map from makeId(Key...) -> DomListenerRegistrations
+        It is used to make sure that double registrations for the same binding do not cause
+        double binding in the front-end. Front end only has one handler per binding.
+     */
     private static HashMap<String, DomListenerRegistration> registrations = new HashMap<>();
     private static final Object lock = new Object();
 
+    /**
+     * Adds a one {@link Key} shortcut for the given  {@link Component}. The provided {@link Listener} is executed
+     * when the shortcut is invoked, the component is attached, and no other component has overridden the shortcut.
+     * <p>
+     *     Previous {@code Component} + {@code Key} combo (if one exists) is overridden by this new one and the given
+     *     {@code Component} now has priority over other registrations on the same {@code Key}.
+     *
+     * @param component     Shortcut owner
+     * @param key           Key used to invoke the shortcut
+     * @param listener      Handler method for the shortcut invocation
+     */
     static public void addShortcut(final Component component, final Key key, final Listener listener) {
         // component is already attached
         if (component.getUI().isPresent()) {
@@ -25,10 +44,33 @@ public final class ShortcutUtil {
             component.addAttachListener(
                     attachEvent -> addHandler(key, component, listener));
             component.addDetachListener(
-                    detachEvent -> removeHandler(key, component));
+                    detachEvent -> removeShortcut(component, key));
         }
     }
 
+    /***
+     * Removes the shortcut identified by {@link Key} {@code key} and owned by {@link Component} {@code component}.
+     *
+     * @param component     Shortcut owner
+     * @param keys          Key used to invoke the shortcut
+     */
+    static public void removeShortcut(Component component, Key keys) {
+        String id = makeId(keys);
+        synchronized (lock) {
+            locked_remove(id, component);
+        }
+    }
+
+    /**
+     * Internals of {@link #addShortcut(Component, Key, Listener)}. Performs the actual shortcut binding.
+     * Removes the previous binding if one exists and registers a new one. Also adds the front-end handler
+     * if one does not exist.
+     *
+     * @param keys
+     * @param component
+     * @param listener
+     * @see #addShortcut(Component, Key, Listener)
+     */
     static private void addHandler(Key keys, Component component, Listener listener) {
         String id = makeId(keys);
         ArrayDeque<HandlerHolder> deque;
@@ -40,6 +82,11 @@ public final class ShortcutUtil {
             } else {
                 deque = scHolders.get(id);
             }
+
+            // remove old shortcut registration for this component
+            // this also means, that re-registering a shortcut for a component causes
+            // that component's shortcut to have priority over other components with
+            // the same shortcut
             locked_remove(id, component);
 
             // add the shortcut handler to the front-end only once
@@ -52,14 +99,12 @@ public final class ShortcutUtil {
         }
     }
 
-    static private void removeHandler(Key keys, Component component) {
-        // TODO: remove the actual key binding, too! :D
-        String id = makeId(keys);
-        synchronized (lock) {
-            locked_remove(id, component);
-        }
-    }
-
+    /**
+     * Inner implementation for {@link #removeShortcut(Component, Key)}. The caller must own the monitor of {@link #lock},
+     * otherwise a runtime exception will be thrown.
+     * @param id            Shortcut identifier generated with {@link #makeId(Key...)}
+     * @param component     Shortcut owner
+     */
     static private void locked_remove(String id, Component component) {
         if (!Thread.holdsLock(lock)) {
             throw new RuntimeException("The thread " + Thread.currentThread().getName() + " does not own the lock. This method cannot be called without the lock");
@@ -77,6 +122,13 @@ public final class ShortcutUtil {
         }
     }
 
+    /**
+     * Invokes the shortcut identified by {@code id}.
+     * Selects the latest addition for this particular id for execution.
+     * TODO: event-bubbling and only-if-focused
+     *
+     * @param id    Shortcut identifier generated with {@link #makeId(Key...)}
+     */
     private static void handleShortCut(String id) {
         synchronized (lock) {
             ArrayDeque<HandlerHolder> deque = scHolders.get(id);
@@ -86,6 +138,13 @@ public final class ShortcutUtil {
         }
     }
 
+    /**
+     * Generates a {@link String} id from the given {@link Key} list. The id is
+     * order agnostic: ctrl+a == a+ctrl
+     *
+     * @param keys  Keys to generate id from
+     * @return An identifier that maps to all key_count! combinations
+     */
     private static String makeId(Key... keys) {
         List<String> keyStrings = new LinkedList<>();
         for (Key k : keys) {
